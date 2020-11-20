306b7b7a58ab2c14224aab389a7e61a5113f2eb7
nikinov <novellenicholas@gmail.com>
2020-11-20 21:09:39 +0100

    prototype1 + Cinema

--- Assets/Scripts/Controllers/Player/Player.cs
+++ Assets/Scripts/Controllers/Player/Player.cs
@@ -3,29 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float Thrust = 1f;
-    private Rigidbody rb;
+    [SerializeField] private bool Player_1;
+    [SerializeField] private float speed = 6f;
+    [SerializeField] private float turnSmoothTime = .1f;
+    
+    private CharacterController _controller;
+    private float _turnSmoothVelocity;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        _controller = gameObject.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float horizontal;
+        float vertical;
+        if (Player_1)
         {
-            rb.AddForce(transform.forward * Thrust);
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            horizontal = Input.GetAxisRaw("HorizontalArrow");
+            vertical = Input.GetAxisRaw("VerticalArrow");
+        }
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            
+            _controller.Move(direction * speed * Time.deltaTime);
         }
     }
 }
