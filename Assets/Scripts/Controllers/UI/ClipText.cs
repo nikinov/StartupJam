aa1895101c800c923e647caf0813dbe5326e889d
nikinov <novellenicholas@gmail.com>
2020-11-21 18:18:46 +0100

    TextClip

--- Assets/Scripts/Controllers/UI/ClipText.cs
+++ Assets/Scripts/Controllers/UI/ClipText.cs
@@ -2,27 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class ClipText : MonoBehaviour
 {
     [SerializeField] private Transform follow;
+    [SerializeField] private Camera cam;
+    
     // Update is called once per frame
     void Update()
     {
-        Vector3 dirFromAtoB = (follow.position - Camera.main.transform.position).normalized;
-        float dotProd = Vector3.Dot(dirFromAtoB, Camera.main.transform.forward);
-
-        if (dotProd > 0.9)
-        {
-            Vector3 namePos = Camera.main.WorldToScreenPoint(follow.position);
-            this.transform.position = namePos;
-            if (gameObject.GetComponent<CanvasGroup>().alpha == 0)
-            {
-                gameObject.GetComponent<CanvasGroup>().alpha = 1;
-            }
-        }
-        else
-        {
-            gameObject.GetComponent<CanvasGroup>().alpha = 0;
-        }
+        Vector3 namePos = cam.WorldToScreenPoint(follow.position);
+        this.transform.position = namePos;
     }
 }
