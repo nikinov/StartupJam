a251de6192fb71772c42218a5d28710bf73a3fbf
nikinov <novellenicholas@gmail.com>
2020-11-21 09:14:03 +0100

    Jump pt1

--- Assets/Scripts/Controllers/Player/Player.cs
+++ Assets/Scripts/Controllers/Player/Player.cs
@@ -9,9 +9,14 @@ public class Player : MonoBehaviour
     [SerializeField] private bool Player_1;
     [SerializeField] private float speed = 6f;
     [SerializeField] private float turnSmoothTime = .1f;
+    [SerializeField] private float jumpSpeed = 10;
+    [SerializeField] private float gravity = 9.81f;
     
     private CharacterController _controller;
     private float _turnSmoothVelocity;
+    private float _directionY;
+    private bool m_IsGrounded;
+    private float m_GroundCheckDistance;
 
     private void Start()
     {
@@ -26,6 +31,15 @@ public class Player : MonoBehaviour
         {
             horizontal = Input.GetAxisRaw("Horizontal");
             vertical = Input.GetAxisRaw("Vertical");
+            if(Input.GetButtonDown("Jump") && _controller.isGrounded)
+            {
+                print("aa");
+                transform.Translate(0, jumpSpeed * Time.deltaTime, 0);
+            }
+            else if (m_IsGrounded)
+            {
+                transform.Translate(0, - gravity * Time.deltaTime, 0);
+            }
         }
         else
         {
@@ -33,7 +47,7 @@ public class Player : MonoBehaviour
             vertical = Input.GetAxisRaw("VerticalArrow");
         }
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
+        
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
@@ -42,5 +56,45 @@ public class Player : MonoBehaviour
             
             _controller.Move(direction * speed * Time.deltaTime);
         }
+        void CheckGroundStatus()
+        {
+            RaycastHit hitInfo;
+#if UNITY_EDITOR
+            // helper to visualise the ground check ray in the scene view
+            Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+#endif
+            // 0.1f is a small offset to start the ray from inside the character
+            // it is also good to note that the transform position in the sample assets is at the base of the character
+            if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+            {
+                m_IsGrounded = true;
+            }
+            else
+            {
+                m_IsGrounded = false;
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
